8a1c2583388f33381b715c1df4d8b46e17374bbb
AkankshaKim <akankshapadave3@gmail.com>
2024-01-12 04:17:01 +0530

    my first commit everthing setup correctly

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Hero , Navbar, Products, Footer} from "./Components"
+
+
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Products/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/Components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Products from "./Products";
+import Footer from "./Footer";
+
+
+export {Navbar, Hero, Products, Footer}
