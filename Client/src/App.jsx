b36d3c2ad35622a9ff43a9458e906a36c3bd2f27
AkankshaKim <akankshapadave3@gmail.com>
2024-01-16 03:30:37 +0530

    adding components to rhe web side

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
-import { Hero , Navbar, Products, Footer} from "./Components"
+import React from "react"
+import { Hero , Navbar, Products, Footer} from "./Components/index"
 
-
+import './App.css'
 
 
 function App() {
